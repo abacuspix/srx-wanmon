02c9b97cc8660e5ac8c44aa91b858611b8973d82
ndavey <ndavey@packetpusher.ca>
2016-10-03 17:26:50 -0700

    broken commit - missing get_stats endpoint

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import time
 import thread
 import time
 
+from flask import jsonify
+
 
 
 from jnpr.junos import Device
@@ -114,7 +116,7 @@ def statLoop (devices):
 
 devices = []
 
-devices.append( Device(host='192.168.57.2', user='root', password='Welcome!') )
+devices.append( Device(host='172.16.237.98', user='root', password='Welcome!') )
 
 for d in devices:
     try:
@@ -128,34 +130,38 @@ thread.start_new_thread( statLoop , (devices,)  )
 
 app = flask.Flask(__name__)
 
+@app.route('/_get_statistics')
+def get_statistics():
+    print "running get statistics api"
+
+    return jsonify( inet_table=device_stats["inet0"]["table"] , inet_route = device_stats["inet0"]["route"] , inet_route_nh = device_stats["inet0"]["nh_if"] , alt_table=device_stats["approute"]["table"] , alt_table_route = device_stats["approute"]["route"] , alt_table_nh = device_stats["approute"]["nh_if"] , gr_if_ibps = device_stats["gr_if"]["ibps"] , gr_if_ipps = device_stats["gr_if"]["ipps"] , gr_if_obps = device_stats["gr_if"]["obps"] , gr_if_opps =  device_stats["gr_if"]["opps"] , st_if_ibps = device_stats["st_if"]["ibps"] , st_if_ipps = device_stats["st_if"]["ipps"] , st_if_obps = device_stats["st_if"]["obps"] , st_if_opps = device_stats["st_if"]["opps"] )
+
 @app.route('/')
-
 def index():
-
-    return_string = ""
-    for i in device_stats:
-        return_string = return_string + i
-        return_string = return_string + "\n"
-
-
     return '''
     <html>
     <head>
     <title> SD-WAN Stats </title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
     </head>
+
     <body>
+
+
     <table>
     <tr>
-    <td><u>''' + device_stats["inet0"]["table"] + '''</u></td>
-    <td><u>''' + device_stats["approute"]["table"] + '''</u></td>
+    <td><u><div id="inettable"></div></u></td>
+    <td><u><div id="alttable"></div></u></td>
     </tr>
     <tr>
-    <td>''' + device_stats["inet0"]["route"] + '''</td>
-    <td>''' + device_stats["approute"]["route"] + '''</td>
+    <td><div id="inetroute"></div></td>
+    <td><div id="altroute"></div></td>
     </tr>
     <tr>
-    <td>''' + device_stats["inet0"]["nh_if"] + '''</td>
-    <td>''' + device_stats["approute"]["nh_if"] + '''</td>
+    <td><div id="inet_route_nh"></div></td>
+    <td><div id="alt_route_nh"></div></td>
     </tr>
     <tr></tr>
     <tr></tr>
@@ -163,33 +169,66 @@ def index():
     <td>
     <table>
 
-    <tr><td><u> ''' + device_stats["inet0"]["nh_if"] + '''</u></tr></td>
-    <tr><td>In BPS: ''' + device_stats["gr_if"]["ibps"]  + ''' </tr></td>
-    <tr><td>In PPS: ''' + device_stats["gr_if"]["ipps"]  + ''' </tr></td>
-    <tr><td>Out BPS: ''' + device_stats["gr_if"]["obps"]  + ''' </tr></td>
-    <tr><td>Out PPS: ''' + device_stats["gr_if"]["opps"]  + ''' </tr></td>
+    <tr><td><u><div id="inet_route_nh"></div></u></tr></td>
+    <tr><td>In BPS: <div id="gr_if_ibps"></div> </tr></td>
+    <tr><td>In PPS: <div id="gr_if_ipps"></div> </tr></td>
+    <tr><td>Out BPS: <div id="gr_if_obps"></div> </tr></td>
+    <tr><td>Out PPS: <div id="gr_if_opps"></div> </tr></td>
 
     </table>
     </td>
     <td>
     <table>
 
-    <tr><td><u> ''' + device_stats["approute"]["nh_if"] + '''</u></tr></td>
-    <tr><td>In BPS: ''' + device_stats["st_if"]["ibps"]  + ''' </tr></td>
-    <tr><td>In PPS: ''' + device_stats["st_if"]["ipps"]  + ''' </tr></td>
-    <tr><td>Out BPS: ''' + device_stats["st_if"]["obps"]  + ''' </tr></td>
-    <tr><td>Out PPS: ''' + device_stats["st_if"]["opps"]  + ''' </tr></td>
+    <tr><td><u><div id="alt_route_nh"></div></u></tr></td>
+    <tr><td>In BPS: <div id="st_if_ibps"></div> </tr></td>
+    <tr><td>In PPS: <div id="st_if_ipps"></div> </tr></td>
+    <tr><td>Out BPS: <div id="st_if_obps"></div> </tr></td>
+    <tr><td>Out PPS: <div id="st_if_opps"></div> </tr></td>
 
     </table>
     </td>
     </tr>
     </table>
-    <body>
+
+
+    <script type=text/javascript>
+
+    setInterval(
+        function()
+            {
+
+                $.getJSON("/_get_statistics",
+                {},
+                function(data) {
+                    $("#inettable").text(data.inet_table);
+                    $("#alttable").text(data.alt_table);
+
+                    $("#inetroute").text(data.inet_route);
+                    $("#altroute").text(data.alt_table_route);
+
+                    $("#inet_route_nh").text(data.inet_route_nh);
+                    $("#alt_route_nh").text(data.alt_table_nh);
+
+                    $("#gr_if_ibps").text(data.gr_if_ibps);
+                    $("#gr_if_obps").text(data.gr_if_obps);
+                    $("#gr_if_ipps").text(data.gr_if_ipps);
+                    $("#gr_if_opps").text(data.gr_if_opps);
+
+                    $("#st_if_ibps").text(data.st_if_ibps);
+                    $("#st_if_obps").text(data.st_if_obps);
+                    $("#st_if_ipps").text(data.st_if_ipps);
+                    $("#st_if_opps").text(data.st_if_opps);
+
+
+
+                });
+            },
+            1000);
+    </script>
+    </body>
     </html>
 
-
-
     '''
-
 
 app.run(debug=True, port=8000, host='0.0.0.0')
